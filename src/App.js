f1abbd6b4dccf507be9437be2fa0d14a0d058b5e
yawwusu <yawwusugh@gmail.com>
2021-06-20 20:18:59 +0200

    Finished spotify login and redirect to dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from "react";
 import Login from "./Login";
 import "./App.css";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
-  return (
-    <div className="App">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = React.useState("");
+  console.log(token);
+
+  React.useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) setToken(_token);
+  }, []);
+
+  return <div className="App">{!!token ? <h1>Dashboard</h1> : <Login />}</div>;
 }
 
 export default App;
