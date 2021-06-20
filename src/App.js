02e9e13eeb1884a0414430e7eae8fdade858f3bf
yawwusu <yawwusugh@gmail.com>
2021-06-20 20:31:43 +0200

    Using SpotifyWebApi kit to get user data
    - Also created Dashboard component for logged in user

--- src/App.js
+++ src/App.js
@@ -2,19 +2,27 @@ import React from "react";
 import Login from "./Login";
 import "./App.css";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Dashboard from "./Dashboard";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = React.useState("");
-  console.log(token);
-
   React.useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-    if (_token) setToken(_token);
+    if (_token) {
+      setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log("user", user);
+      });
+    }
   }, []);
 
-  return <div className="App">{!!token ? <h1>Dashboard</h1> : <Login />}</div>;
+  return <div className="App">{!!token ? <Dashboard /> : <Login />}</div>;
 }
 
 export default App;
