2873ad9e933057fc249756b965fd1ae8cf8cc3ca
yawwusu <yawwusugh@gmail.com>
2021-06-21 13:59:48 +0200

    Added icons and styling to Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,52 @@
 import React from "react";
 import "./Footer.css";
+import { Grid, Slider } from "@material-ui/core";
+import {
+  PlayCircleOutline,
+  PlaylistPlay,
+  Repeat,
+  Shuffle,
+  SkipNext,
+  SkipPrevious,
+  VolumeDown,
+} from "@material-ui/icons";
 
 function Footer() {
   return (
     <div className="Footer">
-      <h1>I am a footer</h1>
+      <div className="Footer-left">
+        <img
+          className="Footer-albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Usher_-_Confessions_album_cover.jpg/220px-Usher_-_Confessions_album_cover.jpg"
+          alt="album cover"
+        />
+        <div className="Footer-songInfo">
+          <h4>Yeah!</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+
+      <div className="Footer-center">
+        <Shuffle className="Footer-green" />
+        <SkipPrevious className="Footer-icon" />
+        <PlayCircleOutline fontSize="large" className="Footer-icon" />
+        <SkipNext className="Footer-icon" />
+        <Repeat className="Footer-green" />
+      </div>
+
+      <div className="Footer-right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlay />
+          </Grid>
+          <Grid item>
+            <VolumeDown />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
